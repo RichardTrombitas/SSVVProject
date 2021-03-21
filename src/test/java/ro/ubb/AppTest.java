d155a3c91bf7442babd81b9534b75ac686b0ddaf
RichardTrombitas <richard.trombitas@yahoo.com>
2021-03-21 15:24:29 +0200

    Implement 2 test cases

--- src/test/java/ro/ubb/AppTest.java
+++ src/test/java/ro/ubb/AppTest.java
@@ -1,20 +1,50 @@
 package ro.ubb;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import ro.ubb.Domain.Student;
+import ro.ubb.Exceptions.ValidatorException;
+import ro.ubb.Repository.XMLFileRepository.StudentXMLRepo;
+import ro.ubb.Service.XMLFileService.StudentXMLService;
+import ro.ubb.Validator.StudentValidator;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.junit.Assert.assertNotNull;
+
+public class AppTest {
+
+    StudentValidator studentValidator;
+    StudentXMLRepo studentXMLRepo;
+    StudentXMLService studentXMLService;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void tc_1_AddStudent() throws ValidatorException {
+        studentValidator = new StudentValidator();
+        studentXMLRepo = new StudentXMLRepo(studentValidator, "StudentiXML_test.xml");
+        studentXMLService = new StudentXMLService(studentXMLRepo);
+
+        String id = "1";
+        String nume = "n1";
+        String grupa = "11";
+        String email = "e1";
+        String prof = "p1";
+        String[] params = {id, nume, grupa, email, prof};
+
+        studentXMLService.add(params);
+        Student addedStudent = studentXMLService.findOne("1");
+        assertNotNull(addedStudent);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void tc_2_AddStudent() throws ValidatorException {
+        studentValidator = new StudentValidator();
+        studentXMLRepo = new StudentXMLRepo(studentValidator, "StudentiXML_test.xml");
+        studentXMLService = new StudentXMLService(studentXMLRepo);
+
+        String id = "1";
+        String nume = "n1";
+        String grupa = "1.23";
+        String email = "e1";
+        String prof = "p1";
+        String[] params = {id, nume, grupa, email, prof};
+        studentXMLService.add(params);
     }
 }
