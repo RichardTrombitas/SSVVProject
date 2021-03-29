f9b8162b7a5b8b3e19c1bd6d0b5e5e0b9cf84c23
RichardTrombitas <richard.trombitas@yahoo.com>
2021-03-29 09:10:21 +0300

    White box tests

---
+++ src/test/java/ro/ubb/WhiteBoxTest.java
@@ -0,0 +1,49 @@
+package ro.ubb;
+
+import org.junit.Test;
+import ro.ubb.Domain.Student;
+import ro.ubb.Exceptions.ValidatorException;
+import ro.ubb.Repository.XMLFileRepository.StudentXMLRepo;
+import ro.ubb.Repository.XMLFileRepository.TemaLabXMLRepo;
+import ro.ubb.Service.XMLFileService.StudentXMLService;
+import ro.ubb.Service.XMLFileService.TemaLabXMLService;
+import ro.ubb.Validator.StudentValidator;
+import ro.ubb.Validator.TemaLabValidator;
+
+import static org.junit.Assert.assertNotNull;
+
+public class WhiteBoxTest {
+    TemaLabValidator temaLabValidator;
+    TemaLabXMLRepo temaLabXMLRepo;
+    TemaLabXMLService temaLabXMLService;
+
+    @Test
+    public void tc_1_AddTemaLab() throws ValidatorException {
+        temaLabValidator = new TemaLabValidator();
+        temaLabXMLRepo = new TemaLabXMLRepo(temaLabValidator, "TemaLabXML_test.xml");
+        temaLabXMLService = new TemaLabXMLService(temaLabXMLRepo);
+
+        String id = "1";
+        String descr = "d1";
+        String saptLim = "11";
+        String saptPred = "7";
+        String[] params={id,descr,saptLim,saptPred};
+
+        temaLabXMLService.add(params);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void tc_2_AddTemaLab() throws ValidatorException {
+        temaLabValidator = new TemaLabValidator();
+        temaLabXMLRepo = new TemaLabXMLRepo(temaLabValidator, "TemaLabXML_test.xml");
+        temaLabXMLService = new TemaLabXMLService(temaLabXMLRepo);
+
+        String id = "1";
+        String descr = "";
+        String saptLim = "11";
+        String saptPred = "7";
+        String[] params={id,descr,saptLim,saptPred};
+
+        temaLabXMLService.add(params);
+    }
+}
